b58c5713d1f2724b787f0e7eca4a0cdbec8ee582
sanbornhnewyyz <amadeus.hilland@gmail.com>
2017-04-05 17:52:39 -0400

    Write some basic todoApp reducer tests

--- test/mainSpec.js
+++
@@ -1,8 +0,0 @@
-import expect from 'expect'
-import { helloWorld } from '../src/main'
-
-describe(`Hello World`, () => {
-  it(`should return 'Hello World'`, () => {
-    expect(helloWorld()).toEqual('Hello World')
-  })
-})
